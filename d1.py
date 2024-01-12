4d0fbea12351f86b4de2b70fa5225ed1c453ffca
kiran gannabathula <137530312+kiran-brainy@users.noreply.github.com>
2024-01-12 10:57:11 +0530

    Update d1.py

--- d1.py
+++ d1.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('puppy')
 st.write('puppy')
-st.image('https://i.pinimg.com/736x/8e/eb/e1/8eebe11cec68e1d4732e71e3f0411cc2.jpg',caption='puppy')
+st.image('https://w0.peakpx.com/wallpaper/109/104/HD-wallpaper-the-iron-man-mark-85-iron-man-superheroes-artist-artwork-digital-art.jpg',caption='puppy')
