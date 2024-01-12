0a74149d2d98c65870bb811806cfb140af19da40
kiran gannabathula <137530312+kiran-brainy@users.noreply.github.com>
2024-01-12 10:48:31 +0530

    Update d1.py

--- d1.py
+++ d1.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('puppy')
 st.write('puppy')
-st.image('https://i.pining.com/564x/36/b4/ef/36b4ef433ac9c2cf9d34e54aa3a38340.jpg',caption='puppy')
+st.image('https://www.pinterest.com/pin/661818107738956832/',caption='puppy')
